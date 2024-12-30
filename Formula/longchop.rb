3462462a13bd4a887002732e5556003f0a754cae
chonkie3 <philp.donald@gmail.com>
2024-12-30 01:22:59 +0000

    Update longchop

--- Formula/longchop.rb
+++ Formula/longchop.rb
@@ -2,7 +2,7 @@ class Longchop < Formula
   desc "CLI tool for displaying and analyzing file contents with tree-like structure"
   homepage "https://github.com/donphi/longchop"
   url "https://github.com/donphi/longchop/archive/v1.0.0.tar.gz"
-  sha256 "dbd54bae2ce72289c3a831349c84f9e15a8aa01fdf18e27b1df0de91ff94498e1" # We'll update this after creating the release
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed" # We'll update this after creating the release
   version "1.0.0"
   
   depends_on "tree"
