df7a6059a07536ce5bc5a9d30832731c75053619
Donald Philp <donaldphilp@Donalds-MacBook-Pro-2.local>
2025-01-19 06:19:45 +0000

    update --exclude function v 1.3.0

--- Formula/longchop.rb
+++ Formula/longchop.rb
@@ -1,9 +1,9 @@
 class Longchop < Formula
   desc "CLI tool for displaying and analyzing file contents with tree-like structure"
   homepage "https://github.com/donphi/longchop"
-  url "https://github.com/donphi/longchop/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "9631d0d5f27068617cc98f1c639be176117bc0c659e050c385dad7692ae57de3" # We'll update this after creating the release
-  version "1.2.0"
+  url "https://github.com/donphi/longchop/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "7b9056a4eafb450757cba3385920473881f81f77238e1ba31c6590d2870956ce" # We'll update this after creating the release
+  version "1.3.0"
   
   depends_on "tree"
 
