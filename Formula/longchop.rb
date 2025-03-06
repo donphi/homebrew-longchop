f0ed2d7ccefaa05f8959b5dd131ab030982fc2ce
donphi <w2107579@westminster.ac.uk>
2025-03-06 09:02:16 +0000

    update 1.4.3

--- Formula/longchop.rb
+++ Formula/longchop.rb
@@ -1,9 +1,9 @@
 class Longchop < Formula
   desc "CLI tool for displaying and analyzing file contents with tree-like structure"
   homepage "https://github.com/donphi/longchop"
-  url "https://github.com/donphi/longchop/archive/refs/tags/v1.4.2.tar.gz"
-  sha256 "194e63d27276bca99d80f054a53fc3a0491b7c5ecedacbd9f9ba576878e57fe7"
-  version "1.4.2"
+  url "https://github.com/donphi/longchop/archive/refs/tags/v1.4.3.tar.gz"
+  sha256 "cd11ecac868a704163bf8cd85a8d02f2afba7506fa10b38f720a640f45787f9a"
+  version "1.4.3"
   
   depends_on "tree"
   def install
