fba1dfbee9e63382c0e5dacd82d9f267a5022919
donphi <w2107579@westminster.ac.uk>
2025-03-13 11:04:10 +0000

    Revert longchop formula to v1.3.0 (commit 263d84f)

--- Formula/longchop.rb
+++ Formula/longchop.rb
@@ -1,14 +1,16 @@
 class Longchop < Formula
   desc "CLI tool for displaying and analyzing file contents with tree-like structure"
   homepage "https://github.com/donphi/longchop"
-  url "https://github.com/donphi/longchop/archive/refs/tags/v1.4.6.tar.gz"
-  sha256 "68d99ed9a2f3254d0f570c55133dfe09bf7edf5bff0ad3c55d3ab482faa1c3cb"
-  version "1.4.6"
+  url "https://github.com/donphi/longchop/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "de122235d49e8fb12886a07ae3b18f4dde76459ee8636e8a5255efa9c5e12c27" # We'll update this after creating the release
+  version "1.3.0"
   
   depends_on "tree"
+
   def install
     bin.install "bin/longchop"
   end
+
   test do
     system "#{bin}/longchop", "--help"
   end
